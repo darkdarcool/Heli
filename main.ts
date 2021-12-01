7d10489d3d9a260cfeed8da4ecb9935d4fd4afbb
Catermelon <66882633+darkdarcool@users.noreply.github.com>
2021-12-01 06:20:14 -0800

    Added donuts

--- main.ts
+++ main.ts
@@ -1,43 +1,3 @@
-function spawnEnemy() {
-    enemy = sprites.create(img`
-        ..............bbbbbbb...........
-        ...........bb66663333baa........
-        .........bb3367776333663aa......
-        ........b33333888333389633aa....
-        .......b3333333333333389633aa...
-        ......b34443333333333338633bae..
-        .....b3455433333333334443333ae..
-        ....b33322333dddd3333455233daee.
-        ...b3d333333dd3bbbb33322333dabe.
-        ..b3d333333d3bb33bb33333333da4e.
-        ..bd33333333b33aab3333333223a4ee
-        .b3d3663333b33aab33366332442b4ee
-        .bd3b983333a3aa3333387633ee3b4ee
-        .bd6983333baaa333333387633bb4bee
-        b3d6833333bba333333333863ba44ebe
-        bdd3333333bb3333333333333a44bebe
-        add666633333322333366333ba44bbbe
-        ad67776333332442336983d3a444b4e.
-        add888b333333ee3369833d3a44b44e.
-        add333333333333336833d3a444b4e..
-        a3dd3333344433333dddd3a444b44e..
-        ab33ddd325543333dd33aa444b44e...
-        .eabb3dd32233333baaa4444b44e....
-        .ebabb3d333d33baa444443b44e.....
-        ..ebaab3ddd3aaa4444433b44e......
-        ..eebbaab33a44444333b444e.......
-        ...eeebbaab444b333b4444e........
-        ....ebeeebbbbbbbb4444ee.........
-        .....eebbbb44444444ee...........
-        .......eeebbb444eee.............
-        ..........eeeeee................
-        ................................
-        `, SpriteKind.Enemy)
-    enemy.y = 0
-    enemy.x = Math.randomRange(0, 200)
-    enemy.setVelocity(0, 50)
-}
-
 sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function (sprite, otherSprite) {
     player.destroy()
     canSpawn = false
@@ -48,7 +8,6 @@ sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function (sprite, otherSp
     })
 });
 let enemysSpawned = 0
-let enemy: Sprite = null
 let canSpawn = false
 let player = sprites.create(img`
     ....ffffff.........ccc..
@@ -75,12 +34,12 @@ game.onUpdate(function () {
     timer.throttle("spawnEnemy", spawnRate, function () {
         if (canSpawn) {
             info.changeScoreBy(1)
-            spawnEnemy()
+            Enemy.spawnIceCream();
             enemysSpawned += 1
-            if (enemysSpawned > 1) {
-                spawnRate += 0 - 10
-                enemysSpawned = 0
-            }
+            spawnRate -= 10
+            Enemy.spawnDonut()
+            
+            
         }
     })
     if (controller.up.isPressed()) {
